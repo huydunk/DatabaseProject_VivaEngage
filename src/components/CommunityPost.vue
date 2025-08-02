fa6fde0ae539d484d1b33429a1ce72418198f5df
huydunk <89574667+huydunk@users.noreply.github.com>
2025-08-03 01:37:43 +0700

    Add #id in Post

--- src/components/CommunityPost.vue
+++ src/components/CommunityPost.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="card mb-4 shadow-sm">
+    <p class="text-muted">#{{post.id}}</p>
     <div class="card-body">
       <h5 class="card-title">{{ post.title }}</h5>
       <p class="card-text">{{ post.content }}</p>
